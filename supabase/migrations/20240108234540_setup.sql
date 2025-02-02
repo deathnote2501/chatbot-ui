f087a217f8aa2f0997dd37805c334a7b8f054b4b
deathnote2501 <107842385+deathnote2501@users.noreply.github.com>
2025-02-02 10:50:25 +0100

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://ffhtrwhipjmzlxrfqdla.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmaHRyd2hpcGptemx4cmZxZGxhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODQ4ODc2MywiZXhwIjoyMDU0MDY0NzYzfQ.Jruoy2Fm6AnR-940ffq09LNKaMz1qOac_Dp7Ks_Jnew'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
